7ff059c3f85884c4ea2b001cde4c7f6e9e2e32f4
Niel Benjamin Kho <benjokho@gmail.com>
2022-05-26 15:03:12 +0800

    Add responsiveness to darkmode

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Header() {
   return (
-<header className="flex justify-between w-full text-fem-light-100 items-center">
-          <h1 className="uppercase tracking-[0.4rem] font-bold align-baseline">Todo</h1>
+<header className="flex justify-between w-full px-1 text-fem-light-100 items-center text-3xl">
+          <h1 className="uppercase tracking-[0.4rem] font-bold align-baseline ">Todo</h1>
           <button className="px-1 rounded group flex justify-center items-center">
-            <i className="fa-solid fa-sun hidden group-hover:-translate-y-[2px] duration-500"></i>
-            <i className="fa-solid fa-moon hiddn group-hover:-translate-y-[2px] duration-500"></i>
+            <i className="fa-solid fa-sun hidden dark:block group-hover:-translate-y-1 duration-500"></i>
+            <i className="fa-solid fa-moon dark:hidden group-hover:-translate-y-1 duration-500"></i>
           </button>
         </header>
   )
