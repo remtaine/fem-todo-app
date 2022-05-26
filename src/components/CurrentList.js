49e4b46e6c8abd90153c227708310bdf59bb1b8f
Niel Benjamin Kho <benjokho@gmail.com>
2022-05-26 15:01:38 +0800

    Add components for ListDetails

--- src/components/CurrentList.js
+++ src/components/CurrentList.js
@@ -1,32 +1,26 @@
 import React from 'react'
-import ButtonsGradient from './ui/ButtonsGradient'
+import ListDetailsDesktop from './ListDetailsDesktop'
+import ListDetailsMobile from './ListDetailsMobile'
+import ListDetailsMobileSubcategory from './ListDetailsMobileSubcategory'
+import ListItem from './ListItem'
 
 function CurrentList() {
     
   return (
-    <section className="rounded shadow bg-fem-light-100 py-2 px-4 w-[420px]">
-    <ul className="flex ">
-      <div className="flex items-center gap-2 border-b-[1px]border-fem-light-300 mb-2 group w-full">
-        <ButtonsGradient />
-        <span className="">Go to the groceries</span>
-        <div className="ml-auto flex gap-2">
-          <button className="ml-auto"><i className="fa-solid fa-pen hidden group-hover:block hover:-translate-y-[2px] ease-in-out duration-500"></i></button>
-          <button className="ml-auto"><i className="fa-solid fa-xmark hidden group-hover:block hover:-translate-y-[2px] ease-in-out duration-500"></i></button>
-        </div>
-      </div>
-    </ul>
-    <div className="text-fem-light-500 flex justify-between text-[0.5rem]">
-      <button className="hover:text-fem-light-400 duration-500">5 items left</button>
-      {/* <span className="mx-1">|</span> */}
-      <div className="flex gap-2  ml-6 flex-nowrap">
-        <button className="hover:text-fem-light-400 duration-500">All</button>
-        <button className="hover:text-fem-light-400 duration-500">Active</button>
-        <button className="hover:text-fem-light-400 duration-500">Completed</button>
-      </div>
-      {/* <span className="mx-1">|</span> */}
-      <button className="hover:text-fem-light-400 duration-500">Clear Completed</button>
+  <section className="w-full">
+    <div className="rounded shadow bg-fem-light-100 dark:bg-fem-dark-200 ">
+      <ul className="flex flex-col">
+        <ListItem />
+        <ListItem text="Walk the dog"/>
+        <ListItem text="Read a book"/>
+      </ul>
+      <ListDetailsDesktop />
+      <ListDetailsMobile />
     </div>
-  </section> 
+    <div className="block sm:hidden">
+      <ListDetailsMobileSubcategory />
+    </div>
+  </section>
   )
 }
 
