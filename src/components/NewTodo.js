23691855fa5dcaa8b9edde5c7e4493f8b47aaee9
Niel Benjamin Kho <benjokho@gmail.com>
2022-05-26 15:03:53 +0800

    Add buttons

--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import ButtonsGradient from './ui/ButtonsGradient'
 
 function NewTodo() {
   return (
-    <section className="flex justify-center items-center py-2 px-4 gap-2 rounded shadow text-sm bg-fem-light-100">
-        <div className="">
-            <input type="text" name="" id="" placeholder="Create a new todo..." className="pr-16 leading-[16px] text-lg pt-1 outline-none"/>
+    <section className="w-full group flex justify-center items-center  px-4 gap-2 rounded shadow text-2xl bg-fem-light-100 dark:bg-fem-dark-200">
+        <ButtonsGradient />
+        <div className="w-full">
+            <input type="text" name="" id="" placeholder="Create a new todo..." className="w-full dark:placeholder-fem-dark-500 text-2xl pt-1 h-16 outline-none dark:bg-fem-dark-200"/>
         </div>
+        <button className="ml-auto"><i className="fa-solid fa-plus hidden group-hover:block hover:-translate-y-[2px] ease-in-out duration-500"></i></button>
     </section>
   )
 }
