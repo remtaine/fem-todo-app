b33e6899556296de48cb5298d11a101743e67d64
Niel Benjamin Kho <benjokho@gmail.com>
2022-05-26 16:36:58 +0800

    Remove opening in new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
         <footer className="text-xs sm:text-sm text-center py-1 ">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="hover:underline font-bold duration-500">Frontend Mentor</a> | Coded by <a href="https://github.com/remtaine" className="hover:underline font-bold duration-500">Ben</a>
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" className="hover:underline font-bold ">Frontend Mentor</a> | Coded by <a href="https://github.com/remtaine" className="hover:underline font-bold ">Ben</a>
         </footer>
   )
 }
