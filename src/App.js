8bbff95223967126e8b893440a061542856bd8e2
Niel Benjamin Kho <benjokho@gmail.com>
2022-05-26 16:37:35 +0800

    Add toggleButton function

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import Background from "./components/Background";
 import Header from "./components/Header";
 
 function App() {
+  const toggleButton = function() {
+    const overall = document.getElementById('overall')
+    overall.classList.toggle('dark');
+    localStorage.theme = overall.classList.contains('dark') ? 'dark' : 'light';
+  };
+
   return (
     <div id="overall" className="h-full w-full">
       <div className="App min-h-screen dark:text-fem-dark-300 bg-fem-light-200 dark:bg-fem-dark-100 flex justify-center flex-col items-center font-josefin selection:bg-fem-light-500 selection:text-fem-light-100">
         <Background />
       
         <section className="flex flex-col items-center justify-center gap-2 p-8 w-full mx-2 max-w-[600px] -translate-y-2 min-h-[calc(100vh-2rem)]">
-          <Header />
+          <Header toggleButton={toggleButton}/>
           <NewTodo />
           <CurrentList />
           <span className="text-xs font-light text-fem-light-400 dark:text-fem-dark-500">Drag and drop to reorder list</span>
