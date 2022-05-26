9f0c68775887e4a7be0fb5ac2ba7061cf3cfe920
Niel Benjamin Kho <benjokho@gmail.com>
2022-05-26 15:01:02 +0800

    Add dark mode images

--- src/components/Background.js
+++ src/components/Background.js
@@ -6,9 +6,15 @@ import BgMobileDark from "../../src/images/bg-mobile-dark.jpg";
 
 function Background() {
   return (
-<div className="absolute top-0 h-[35%]">
-        <img src={BgDesktopLight} alt="" className="hidden mobile:block object-cover min-h-full"/>
-        <img src={BgMobileLight} alt="" className="block mobile:hidden object-cover min-h-full"/>
+<div className="absolute top-0 h-2/5">
+        <div className='w-full h-full block dark:hidden'>
+          <img src={BgDesktopLight} alt="" className="hidden mobile:block object-cover min-h-full"/>
+          <img src={BgMobileLight} alt="" className="block mobile:hidden object-cover min-h-full"/>
+        </div>
+        <div className='w-full h-full hidden dark:block'>
+          <img src={BgDesktopDark} alt="" className="hidden mobile:block object-cover min-h-full"/>
+          <img src={BgMobileDark} alt="" className="block mobile:hidden object-cover min-h-full"/>
+        </div>
       </div>
   )
 }
