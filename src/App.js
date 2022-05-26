6954c214276d0d1e79b6e9ae4634e32b3cbaa073
Niel Benjamin Kho <benjokho@gmail.com>
2022-05-26 15:01:29 +0800

    Add dark mode

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App h-screen bg-fem-light-200 flex justify-center flex-col items-center font-josefin selection:bg-fem-light-500 selection:text-fem-light-100">
-      <Background />
-    
-      <section className="flex flex-col items-center justify-center gap-2 p-8 w-5/6 lg:w-[500px] -translate-y-2 min-h-[calc(100vh-1.5rem)]">
-        <Header />
-        <NewTodo /> 
-        <CurrentList />
-
-        <span className="text-[0.5rem] font-light text-fem-light-400">Drag and drop to reorder list</span>
-      </section>
-
-      <Footer />
+    <div className="dark h-full w-full">
+      <div className="App min-h-screen dark:text-fem-dark-300 bg-fem-light-200 dark:bg-fem-dark-100 flex justify-center flex-col items-center font-josefin selection:bg-fem-light-500 selection:text-fem-light-100">
+        <Background />
+      
+        <section className="flex flex-col items-center justify-center gap-2 p-8 w-full mx-2 max-w-[600px] -translate-y-2 min-h-[calc(100vh-2rem)]">
+          <Header />
+          <NewTodo />
+          <CurrentList />
+          <span className="text-xs font-light text-fem-light-400 dark:text-fem-dark-500">Drag and drop to reorder list</span>
+        </section>
+        <Footer />
+      </div>
     </div>
   );
 }
