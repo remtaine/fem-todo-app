0ec4b5aaf2ed4cf772bb93c93d80f645e284ae9c
Niel Benjamin Kho <benjokho@gmail.com>
2022-05-26 16:37:17 +0800

    Fix styling

--- src/components/ListDetailsMobileSubcategory.js
+++ src/components/ListDetailsMobileSubcategory.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ListDetailsMobileSubcategory() {
   return (
-    <div className="flex gap-4 py-3 my-4 rounded-md shadow flex-nowrap justify-center text-fem-dark-600 bg-fem-light-100 dark:bg-fem-dark-200">
+    <div className="flex gap-4 pt-4 pb-3 my-4 rounded-md shadow flex-nowrap justify-center text-fem-dark-600 bg-fem-light-100 dark:bg-fem-dark-200">
         <button className="text-fem-100 duration-500">All</button>
         <button className="hover:text-fem-light-400 duration-500">Active</button>
         <button className="hover:text-fem-light-400 duration-500">Completed</button>
