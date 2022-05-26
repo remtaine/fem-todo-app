9bf09389b79377dfc527be2a1b593848c97e03d2
Niel Benjamin Kho <benjokho@gmail.com>
2022-05-26 15:01:46 +0800

    Make footer text responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
-        <footer className="text-[0.5rem] text-center py-1">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="underline hover:font-bold duration-500">Frontend Mentor</a> | Coded by <a href="https://github.com/remtaine" className="underline hover:font-bold duration-500">Ben</a>
+        <footer className="text-xs sm:text-sm text-center py-1 ">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="hover:underline font-bold duration-500">Frontend Mentor</a> | Coded by <a href="https://github.com/remtaine" className="hover:underline font-bold duration-500">Ben</a>
         </footer>
   )
 }
